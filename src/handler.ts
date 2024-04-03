5654ae9c5ec0726f7bf2dfe366c80559a1933f24
lutfi angga <lutfiangg20@gmail.com>
2024-04-04 06:51:21 +0700

    first commit

---
+++ src/handler.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+/* type data = {
+  nama_barang: string;
+  kategori: string;
+  stok: number;
+  harga: number;
+}; */
+
+export const postBarang = async (data: {
+  nama_barang: string;
+  kategori: string;
+  stok: number;
+  harga: number;
+}) => {
+  try {
+    const barang = await prisma.barangs.create({ data: data });
+    console.log(barang);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getBarang = async () => {
+  try {
+    const barang = await prisma.barangs.findMany();
+    console.log(barang);
+    return barang;
+  } catch (err) {
+    console.log(err);
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import { Elysia } from "elysia";
+import { PrismaClient } from "@prisma/client";
+import { postBarang, getBarang } from "./handler";
+const prisma = new PrismaClient();
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+type data = {
+  nama_barang: string;
+  kategori: string;
+  stok: number;
+  harga: number;
+};
+
+const app = new Elysia({ prefix: "/api" })
+  .get("/", () => "Hello Elysia")
+  .get("/barang", () => {
+    return getBarang();
+  })
+  .post("/barang", (req: { body: data }) => {
+    postBarang(req.body);
+    return req.body;
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
